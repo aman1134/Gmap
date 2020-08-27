35c3e45872859ce98167b63ec7cbbb606eb575b7
Aman Rastogi <aman.rastogi.90281@gmail.com>
2020-08-27 12:39:56 +0530

    Aim and done button added

--- app/src/main/java/com/example/lattice/MapsActivity.java
+++ app/src/main/java/com/example/lattice/MapsActivity.java
@@ -12,9 +12,14 @@ import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,7 +33,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback , GoogleMap.OnMapClickListener , View.OnClickListener {
 
     private GoogleMap mMap;
     private static int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1 ;
@@ -37,7 +42,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private String TAG = "map";
     private LatLng defaultLocation = new LatLng(34.765 , 74.2546);
     FusedLocationProviderClient fusedLocationProviderClient;
-    private Location lastKnownLocation;
+    private Location lastKnownLocation; // Variable to store the last known location
+    Marker currLocationMarker; // Current Location Marker to point where are you
 
 
     @Override
@@ -49,34 +55,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        ImageView aim = (ImageView) findViewById(R.id.aim);
+        ImageView done = (ImageView) findViewById(R.id.done);
+        aim.setOnClickListener(this);
+        done.setOnClickListener(this);
+
 
         //Check the loaction permission
         checkLocaionPermission();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        // Add a marker in Sydney and move the camera
+
+        mMap.setOnMapClickListener(this);
+        //Getting the current location of the device
+        getDeviceLocation();
     }
 
     
     private void checkLocaionPermission() {
-
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             locationPermissionGranted = true;
-        } else
+        } else{
             ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                     PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
@@ -94,10 +101,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             locationPermissionGranted = true;
         }
-        updateLocationUI();
-    }
-
-    private void updateLocationUI() {
+        getDeviceLocation();
     }
 
 
@@ -108,6 +112,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
          */
         try {
             if (locationPermissionGranted) {
+                if(currLocationMarker != null)
+                    currLocationMarker.remove();
                 Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
                 locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
                     @Override
@@ -126,7 +132,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                         lastKnownLocation.getLongitude()));
                                 markerOptions.title("YOU");
                                 markerOptions.icon(icon);
-                                mMap.addMarker(markerOptions);
+                                currLocationMarker = mMap.addMarker(markerOptions);
                             }
                         } else {
                             Log.d(TAG, "Current location is null. Using defaults.");
@@ -143,4 +149,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        if(currLocationMarker != null)
+            currLocationMarker.remove();
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom( latLng , 17));
+        BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.marker);
+        MarkerOptions options = new MarkerOptions();
+        options.position( latLng);
+        options.title("YOU");
+        options.icon(icon);
+        currLocationMarker = mMap.addMarker(options);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.aim :
+                getDeviceLocation();
+                break;
+
+            case R.id.done:
+                Toast.makeText(this, "Your Location is recorded", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
